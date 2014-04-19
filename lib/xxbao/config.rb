602c6796ecf10ee62c926560f842b19e626da0f9
Spirit <neverlandxy.naix@gmail.com>
2014-04-20 01:31:10 +0800

    搞不定中文表格, 换英文版

--- lib/xxbao/config.rb
+++ lib/xxbao/config.rb
@@ -1,45 +1,40 @@
 # coding: utf-8
 
 module Xxbao
-  @base  = 'http://jingzhi.funds.hexun.com/'
-  @regex = /<font.+><span>(\d\.\d+)<\/span>.+\r\n.+<font.+>(\d\.\d+%)<\/font>/
-  @funds = {
-    "000343" => {
-      bao: "财富宝",
-      platform: "微信理财通",
-      fund: "华夏财富宝货币"
-    },
-    "000397" => {
-      bao: "全额宝",
-      platform: "微信理财通",
-      fund: "汇添富全额宝货币"
-    },
-    "000330" => {
-      bao: "现金宝",
-      platform: "网易理财",
-      fund: "汇添富现金宝货币"
-    },
-    "000198" => {
-      bao: "余额宝",
-      platform: "支付宝",
-      fund: "天弘增利宝货币"
-    },
-    "003003" => {
-      bao: "百赚",
-      platform: "百度理财",
-      fund: "华夏财富宝货币"
-    },
-    "000464" => {
-      bao: "百赚利滚利版",
-      platform: "百度理财",
-      fund: "嘉实活期宝货币"
-    },
-    "000389" => {
-      bao: "零钱宝",
-      platform: "苏宁",
-      found: "广发天天红货币"
+  module Config
+    BaseUrl = 'http://jingzhi.funds.hexun.com/'
+    Regex   = /<font.+><span>(\d\.\d+)<\/span>.+\r\n.+<font.+>(\d\.\d+%)<\/font>/
+    Funds   = {
+      "000343" => {
+        bao: "CaiFuBao",
+        platform: "WeChat",
+      },
+      "000397" => {
+        bao: "QuanEBao",
+        platform: "WeChat",
+      },
+      "000330" => {
+        bao: "XianJinBao",
+        platform: "Netease",
+      },
+      "000198" => {
+        bao: "YuEBao",
+        platform: "Alipay",
+      },
+      "003003" => {
+        bao: "BaiZhuan",
+        platform: "Baidu",
+      },
+      "000464" => {
+        bao: "BaiZhuan(ligunli)",
+        platform: "Baidu",
+      },
+      "000389" => {
+        bao: "LingQianBao",
+        platform: "Suning",
+      }
     }
-  }
+  end
 end
 
 
