2dbfdb3a853699eff5f9428c961e8061f7c7fe7a
Spirit <neverlandxy.naix@gmail.com>
2014-04-20 01:32:12 +0800

    增加command_line_reporter

--- lib/xxbao.rb
+++ lib/xxbao.rb
@@ -2,24 +2,56 @@
 
 require "xxbao/version"
 require "xxbao/config"
-require "thor"
-require "formatador"
-require "colorize"
-require 'net/http'
+
+require "net/http"
+require "command_line_reporter"
 
 module Xxbao
+  class Base
+    include Config
+    include CommandLineReporter
 
-  class << self
-    def fetch code, fund
-      uri = URI(@base + "#{code}.shtml")
-      if Net::HTTP.get(uri) =~ @regex
-        fund[:per_million] ||= $1
-        fund[:seven_yield] ||= $2
+    def initialize
+      @funds = Config::Funds
+      fetch_funds
+    end
+
+    def run
+      table(border: true) do
+        row do
+          column('Name', width: 17)
+          column('Platform', width: 10)
+          column('SevenYearRateOfReturn', width: 22)
+          column('PerMillionFundNetRevenue', width: 24)
+        end
+        @funds.values.each do |f|
+          row do
+            column(f[:bao])
+            column(f[:platform])
+            column(f[:seven_yield])
+            column(f[:per_million])
+          end
+        end
       end
     end
-  end
 
-  @funds.map do |code, fund|
-    Thread.new { fetch(code, fund) }
-  end.each { |t| t.join }
+    private
+
+      def fetch_funds
+        @funds.map do |code, fund|
+          Thread.new { fetch_fund(code, fund) }
+        end.each { |t| t.join }
+      end
+
+      def fetch_fund code, fund
+        uri = URI(Config::BaseUrl + "#{code}.shtml")
+        if Net::HTTP.get(uri) =~ Regex
+          fund[:per_million] ||= $1
+          fund[:seven_yield] ||= $2
+        end
+      end
+
+  end
 end
+
+Xxbao::Base.new.run
--- xxbao.gemspec
+++ xxbao.gemspec
@@ -8,7 +8,7 @@ Gem::Specification.new do |spec|
   spec.version       = Xxbao::VERSION
   spec.authors       = ["Spirit"]
   spec.email         = ["neverlandxy.naix@gmail.com"]
-  spec.summary       = %q{A commandline-tool to check the xxbao fund income}
+  spec.summary       = %q{xxbao commandline-tool}
   spec.description   = %q{A commandline-tool to check the xxbao fund income}
   spec.homepage      = "https://github.com/NaixSpirit/xxbao"
   spec.license       = "MIT"
@@ -19,10 +19,8 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.5"
-  spec.add_development_dependency "rspec"
-  spec.add_development_dependency "rake"
+  spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency "rspec", "~> 2.14"
 
-  spec.add_dependency "thor"
-  spec.add_dependency "formatador"
-  spec.add_dependency "colorize"
+  spec.add_dependency "command_line_reporter", "~> 3.3"
 end
