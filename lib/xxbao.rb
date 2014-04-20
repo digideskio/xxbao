96749ab26aaa69ad3e8b94b51e004a0cb03db70c
Spirit <neverlandxy.naix@gmail.com>
2014-04-20 14:17:21 +0800

    add bin

--- lib/xxbao.rb
+++ lib/xxbao.rb
@@ -16,23 +16,11 @@ module Xxbao
       fetch_funds
     end
 
-    def run
-      table(border: true) do
-        row do
-          column('Name', width: 17)
-          column('Platform', width: 10)
-          column('SevenYearRateOfReturn', width: 22)
-          column('PerMillionFundNetRevenue', width: 24)
-        end
-        @funds.values.each do |f|
-          row do
-            column(f[:bao])
-            column(f[:platform])
-            column(f[:seven_yield])
-            column(f[:per_million])
-          end
-        end
-      end
+    def output
+      @table = CommandLineReporter::Table.new(border: true, width: :auto)
+      @table.add(first_row)
+      @funds.values.each { |f| @table.add(fund_row(f)) }
+      @table.output
     end
 
     private
@@ -51,7 +39,35 @@ module Xxbao
         end
       end
 
+      def first_row
+        row = CommandLineReporter::Row.new(bold: true)
+        [
+          CommandLineReporter::Column.new('Name', align: 'center'),
+          CommandLineReporter::Column.new('Platform', align: 'center'),
+          CommandLineReporter::Column.new('SevenYearRateOfReturn', align: 'center'),
+          CommandLineReporter::Column.new('PerMillionFundNetRevenue', align: 'center')
+        ].each { |c| row.add(c) }
+        row
+      end
+
+      def fund_row fund
+        row = CommandLineReporter::Row.new
+        [
+          CommandLineReporter::Column.new(fund[:bao]),
+          CommandLineReporter::Column.new(fund[:platform]),
+          CommandLineReporter::Column.new(fund[:seven_yield]),
+          CommandLineReporter::Column.new(fund[:per_million])
+        ].each { |c| row.add(c) }
+        row
+      end
+
+    # patch for CommandLineReporter
+    # a row will not inherit bold & color from other row
+    class ::CommandLineReporter::Table
+      def inherit_column_attrs(row)
+        row.columns.each_with_index { |c, i| use_positional_attrs(c, i) }
+      end
+    end
+
   end
 end
-
-Xxbao::Base.new.run
